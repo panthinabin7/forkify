5435d344a1de6c903c1834db5b1c77958f8dbada
panthinabin7 <panthinabin7@gmail.com>
2022-09-29 08:56:31 +0545

    cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -126,10 +126,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes); // by this we implemented publisher sybscriber pattern
@@ -138,6 +134,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
